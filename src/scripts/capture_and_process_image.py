e8c52127acf6e16aac6682d0e2636c6ebef751d8
Thomas Cunningham <tryanc15@gmail.com>
2023-05-03 17:35:31 -0500

    Add data export

--- src/scripts/capture_and_process_image.py
+++ src/scripts/capture_and_process_image.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-
 import rospy
+import cv2
+import os
+import numpy as np
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from std_srvs.srv import Trigger, TriggerResponse
 from fiducial_msgs.msg import FiducialTransformArray
 from kinova_5551_chess_arm.msg import ArucoPositions, ArucoPosition
-from kinova_5551_chess_arm.srv import CaptureAndProcessImage, CaptureAndProcessImageResponse
+from kinova_5551_chess_arm.srv import CaptureAndProcessImage, CaptureAndProcessImageResponse, ExportData, ExportDataResponse
 
 
 class CaptureAndProcessImageNode:
@@ -18,6 +20,7 @@ class CaptureAndProcessImageNode:
         self.aruco_sub = rospy.Subscriber('/fiducial_transforms', FiducialTransformArray, self.aruco_callback)
         self.aruco_positions_pub = rospy.Publisher('aruco_positions', ArucoPositions, queue_size=10)
         self.capture_and_process_image_service = rospy.Service('capture_and_process_image', CaptureAndProcessImage, self.handle_capture_and_process_image)
+        self.export_data_service = rospy.Service('export_data', ExportData, self.capture_image_and_export_data)
 
     def image_callback(self, msg):
         # Store the latest image
@@ -48,6 +51,30 @@ class CaptureAndProcessImageNode:
         response = CaptureAndProcessImageResponse()
         response.success = True
         return response
+    
+    def capture_image_and_export_data(self, req):
+        if not hasattr(self, 'latest_image'):
+            return ExportDataResponse(success=False, message='No image received yet')
+
+        if not hasattr(self, 'latest_aruco_transforms'):
+            return ExportDataResponse(success=False, message='No ArUco transforms received yet')
+
+        aruco_positions = [(t.fiducial_id, t.transform.translation.x, t.transform.translation.y, t.transform.translation.z) for t in self.latest_aruco_transforms]
+
+        # Convert the ROS image to an OpenCV image
+        cv_image = self.bridge.imgmsg_to_cv2(self.latest_image, desired_encoding='bgr8')
+
+        # Save the image to a file
+        image_filename = os.path.join(os.path.expanduser('~'), 'aruco_image.png')
+        cv2.imwrite(image_filename, cv_image)
+
+        # Save the ArUco positions to a text file
+        positions_filename = os.path.join(os.path.expanduser('~'), 'aruco_positions.txt')
+        with open(positions_filename, 'w') as f:
+            for pos in aruco_positions:
+                f.write(f'{pos[0]} {pos[1]} {pos[2]} {pos[3]}\n')
+
+        return ExportDataResponse(success=True, message=f'Image and positions exported to {image_filename} and {positions_filename}')
 
 if __name__ == '__main__':
     # Create a CaptureAndProcessImageNode instance and start the ROS node
